b9ab514740189bf8c4a52b087bdf97024d27939d
Andrew Sergeev <corop@list.ru>
2012-11-09 20:04:18 +0400

    initial

---
+++ complaints-card/src/main/java/ru/fccland/complaints/card/service/ComplaintAuthorService.java
@@ -0,0 +1,18 @@
+package ru.fccland.complaints.card.service;
+
+import ru.fccland.complaints.card.form.ComplaintAuthor;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asergeev
+ * Date: 09.11.12
+ * Time: 19:57
+ * To change this template use File | Settings | File Templates.
+ */
+public interface ComplaintAuthorService {
+    public void add(ComplaintAuthor complaintAuthor);
+    public List<ComplaintAuthor> list();
+    public void remove(Long id);
+}
---
+++ complaints-card/src/main/java/ru/fccland/complaints/card/service/impl/ComplaintAuthorServiceImpl.java
@@ -0,0 +1,42 @@
+package ru.fccland.complaints.card.service.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import ru.fccland.complaints.card.dao.ComplaintAuthorDAO;
+import ru.fccland.complaints.card.form.ComplaintAuthor;
+import ru.fccland.complaints.card.service.ComplaintAuthorService;
+
+import java.util.List;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: asergeev
+ * Date: 09.11.12
+ * Time: 20:01
+ * To change this template use File | Settings | File Templates.
+ */
+@Service
+public class ComplaintAuthorServiceImpl implements ComplaintAuthorService {
+    @Autowired
+    private ComplaintAuthorDAO complaintAuthorDAO;
+
+    @Transactional
+    @Override
+    public void add(ComplaintAuthor complaintAuthor) {
+        complaintAuthorDAO.add(complaintAuthor);
+    }
+
+    @Transactional
+    @Override
+    public List<ComplaintAuthor> list() {
+        return complaintAuthorDAO.list();
+    }
+
+    @Transactional
+    @Override
+    public void remove(Long id) {
+        complaintAuthorDAO.remove(id);
+    }
+
+}
